dac98de16a70750d5551606873d5ba2e896dc2a0
Bradley Mwangangi <237662477+bradmwangangi@users.noreply.github.com>
2026-01-22 00:56:26 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="max-w-5xl mx-auto p-8 text-center">
+      <img
+        src="/calendar.svg"
+        className="h-24 p-6 mx-auto"
+        alt="Calendar logo"
+      />
+      <h1 className="text-4xl font-bold mb-8">Calendar</h1>
+    </div>
+  )
+}
+
+export default App
